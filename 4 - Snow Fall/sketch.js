562747a413da7732afbd8a23867cd2d39bab3bba
Álvaro Lázaro Sevilla <alseether@users.noreply.github.com>
2018-10-18 17:31:46 +0200

    some improvements

--- 4 - Snow Fall/sketch.js
+++ 4 - Snow Fall/sketch.js
@@ -6,8 +6,8 @@ var gr;
 function setup(){
 	createCanvas(600,400);
 	gr = createVector(0, 0.05);
-	for(var i=0; i < 20; i++){
-		flakes[i] = new SnowFlake(random(width), random(-20,-10), random(5,15));
+	for(var i=0; i < 30; i++){
+		flakes[i] = new SnowFlake(random(width), random(-60,-10), min(random(2,15), random(2,15)));
 	}
 }
 
@@ -28,6 +28,7 @@ function SnowFlake(x, y, radius){
 	this.vel = createVector(0,0);
 	this.acc = createVector(0,0);
 	this.branches = 2 * round(random(3,4));
+	this.angle = TWO_PI / this.branches;
 	this.r = radius;
 
 	this.applyForce = function(force){
@@ -44,34 +45,40 @@ function SnowFlake(x, y, radius){
 	this.show = function(){
 		stroke(255);
 		noFill();
+		var agl = 0;
 		for(var i = 0; i < this.branches; i++){
-			var a = (TWO_PI * i) / this.branches;
-			this.drawArm(this.pos, this.r, a);
+			this.drawArm(this.pos, this.r, agl, 0);
+			agl += this.angle;
 		}
 	}
 
-	this.drawArm = function(origin, length, angle){
-		if(length < 1){
+	this.drawArm = function(origin, length, angle, recLevel){
+		if(length < (this.r / 5) || recLevel >= 2){
 			return;
 		}
-		var endX = length * (cos(angle));
-		var endY = length * (sin(angle));
+		var cosa = (cos(angle));
+		var sina = (sin(angle));
+		var endX = length * cosa;
+		var endY = length * sina;
 		line(origin.x, origin.y, origin.x + endX, origin.y + endY);
 		var step = length / 5;
-		for(var i=0; i < length; i+=step){
-			var origX = origin.x + i * (cos(angle));
-			var origY = origin.y + i * (sin(angle));
-			this.drawArm(createVector(origX, origY), length / 3, angle - QUARTER_PI);
-			this.drawArm(createVector(origX, origY), length / 3, angle + QUARTER_PI);
+		var end = length-step;
+
+		for(var i=0; i < end; i+=step){
+			var origX = origin.x + i * cosa;
+			var origY = origin.y + i * sina;
+			this.drawArm(createVector(origX, origY), length / 3, angle - QUARTER_PI, recLevel+1);
+			this.drawArm(createVector(origX, origY), length / 3, angle + QUARTER_PI, recLevel+1);
 		}
 	}
 
 	this.reset = function(){
-		this.pos = createVector( random(width), random(-20,-10) );
+		this.pos = createVector( random(width), random(-60,-10) );
 		this.vel = createVector(0,0);
 		this.acc = createVector(0,0);
 		this.branches = 2 * round(random(3,4));
-		this.r = random(20,50);
+		this.angle = TWO_PI / this.branches;
+		this.r = min(random(2,15), random(2,15));
 	}
 
 	this.isOutOfScreen = function(){
